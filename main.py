fa529b992dbba146cf5092a24f598fa99d45febd
Coolpic 783 <coolpic784@gmail.com>
2020-10-21 01:42:50 +0000

    saved current version

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import discord
 import os
 from flask import Flask
 from threading import Thread
+import random
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$')
 
 #Flask Setup
-
 app = Flask('')
 
 
@@ -38,15 +41,41 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    hello = 'no variable yet!'
+    greeting = random.randint(0, 5)
+    if greeting == 1:
+        hello = 'Hi!'
+    if greeting == 2:
+        hello = 'Hi There!'
+    if greeting == 2:
+        hello = 'Hello!'
+    if greeting == 3:
+        hello = 'Hello There!'
+    if greeting == 4:
+        hello = 'Hey There!'
+    if greeting == 5:
+        hello = 'Howdy!'
     if message.author == client.user:
         return
-
+    # each message has a bunch of attributes. Here are a few.
+    # check out more by print(dir(message)) for example.
+    print(
+        f"{message.channel}: {message.author}: {message.author.name}: {message.content}"
+    )
     if message.content.startswith('$hello'):
-        await message.channel.send('Hi!')
-
+        await message.channel.send(hello)
+    if str(message.author) == "Coolpic#0882":
+        if 'Who Are You?' in message.content:
+            await message.channel.send("I'm you.")
+        if 'Get us the link please' in message.content:
+            await message.channel.send("Sure thing!")
+            await message.channel.send("https://repl.it/join/frkmiakl-coolpic783")
+    if "How many people are here?" in message.content:
+        GamingAndChill = client.get_guild(653627118248263701)
+        await message.channel.send(f"```{GamingAndChill.member_count}```")
+        if 'a'
 
 #Run
-
 keep_alive()
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
